965510623898fd9e5676637d748547bf109f30bc
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-02-06 17:20:52 +1300

    Add Maven project, plugin skeleton and IDE config
    Initialize project with a Maven build and basic Spigot plugin skeleton: added pom.xml (maven-compiler + maven-shade, spigot repository and provided spigot-api dependency, java.version), a minimal JavaPlugin subclass (org.voxelhorizons.ItemCore) and plugin.yml. Also added IntelliJ project files (.idea/*) including module/facet, compiler/annotation processing, encodings, remote repositories and misc settings.

---
+++ src/main/java/org/voxelhorizons/ItemCore.java
@@ -0,0 +1,17 @@
+package org.voxelhorizons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

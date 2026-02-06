59f74316fb25463daafb4e1fb8c051f701978d83
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-02-06 20:49:46 +1300

    Add singleton instance and CI safeguard
    Introduce a static PluginCore.instance with a getInstance() accessor and a constructor guard that throws IllegalStateException if initialized twice to prevent multiple plugin instances. Preserve CI-mode startup in onEnable() and add a DO NOT REMOVE comment explaining it prevents the GitHub Action from getting stuck.

--- src/main/java/org/voxelhorizons/PluginCore.java
+++ src/main/java/org/voxelhorizons/PluginCore.java
@@ -5,9 +5,23 @@ import org.voxelhorizons.debug.CIMode;
 
 public final class PluginCore extends JavaPlugin {
 
+    public static PluginCore instance;
+    public static PluginCore getInstance() {
+        return instance;
+    }
+
+    public PluginCore() {
+        if(this.instance != null) {
+            throw new IllegalStateException(getName() + " already initialized!");
+        }
+        this.instance = this;
+    }
+
     @Override
     public void onEnable() {
         // Instantiate and start CI-mode auto shutdown
+        // DO NOT REMOVE
+        // This helps prevent the Github Action from getting stuck
         new CIMode(this).start();
     }
 
